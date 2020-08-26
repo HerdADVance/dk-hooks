1ae3129043fd340d05ed1a6dda9daf1a87f8bb49
Alex <avance@bulldogcreative.com>
2020-08-26 16:41:25 -0400

    Date formatted

--- src/util/optimizeLineupStartTimes.js
+++ src/util/optimizeLineupStartTimes.js
@@ -5,13 +5,26 @@ const optimizeLineupStartTimes = (l, p) => {
     for(var i = 0; i < l.length; i++){
 
         let rosterPlayers = []
+        let positions = []
+        let dates = []
 
         for(var j = 0; j < l[i].roster.length; j++){
             let rosterPlayer = p[l[i].roster[j].player]
             rosterPlayers.push(rosterPlayer)
+            positions.push(rosterPlayer.positions)
+            //08/09/2020 05:00PM ET
+            dates.push(new Date('08/09/2020 12:30'))
         }
 
         console.log(rosterPlayers)
+        console.log(positions)
+        console.log(dates)
+
+        dates.sort(function compare(a, b) {
+            return a - b
+        });
+
+        console.log(dates)
 
         if (i === 0) break
 
